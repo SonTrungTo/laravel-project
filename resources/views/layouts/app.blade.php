554246283e9795fbded1e01803c7985d3bea3294
SonTrungTo <son.trung.to@gmail.com>
2022-04-18 22:24:57 +0300

    front-end in progress...

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,17 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex flex-row align-items-center" href="{{ url('/') }}">
+                    <div style="padding-right: 10px; border-right: solid 1px #333333;">
+                        <img
+                          src="/svg/InstagramIcon.svg"
+                          width="20" height="20"
+                          alt="InstaIcon"
+                        />
+                    </div>
+                    <div style="padding-left: 10px;">
+                        {{ config('app.name', 'Laravel') }}
+                    </div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
